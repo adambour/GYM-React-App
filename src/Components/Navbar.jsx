90a69eadb010a97d300cfe617476136fbea4eadc
Badam <bouradam.adam@gmail.com>
2024-09-24 15:52:55 +0100

    Navbar toggle fix

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,33 +3,33 @@ import logo from '../images/logo.png';
 import { Link } from 'react-scroll';
 
 function Navbar() {
-    const [nav, setnav] = useState(false);
+    const [nav, setNav] = useState(false);
+
     const changeBackground = () => {
         if (window.scrollY >= 50) {
-            setnav(true);
-        }
-        else {
-            setnav(false);
+            setNav(true);
+        } else {
+            setNav(false);
         }
     }
+
     window.addEventListener('scroll', changeBackground);
+
     return (
-        <nav className={nav ? " nav active" : "nav"}>
-            <Link to='main' className='logo'smooth={true} duration={2000}>
+        <nav className={nav ? "nav active" : "nav"}>
+            <Link to='main' className='logo' smooth={true} duration={2000}>
                 <img src={logo} alt='' />
             </Link>
             <input className='menu-btn' type='checkbox' id='menu-btn' />
-            <label className='menu-icon' htmlfor='menu-btn'>
-                <span className='nav-icon'>
-
-                </span>
+            <label className='menu-icon' htmlFor='menu-btn'>
+                <span className='nav-icon'></span>
             </label>
             <ul className='menu'>
-                <li><Link to='main'smooth={true} duration={1000}>Header</Link></li>
-                <li><Link to='features'smooth={true} duration={1000}>Features</Link></li>
-                <li><Link to='offer'smooth={true} duration={1000}>Offre</Link></li>
-                <li><Link to='about'smooth={true} duration={1000}>About</Link></li>
-                <li><Link to='contact'smooth={true} duration={1000}>Contact</Link></li>
+                <li><Link to='main' smooth={true} duration={1000}>Header</Link></li>
+                <li><Link to='features' smooth={true} duration={1000}>Features</Link></li>
+                <li><Link to='offer' smooth={true} duration={1000}>Offre</Link></li>
+                <li><Link to='about' smooth={true} duration={1000}>About</Link></li>
+                <li><Link to='contact' smooth={true} duration={1000}>Contact</Link></li>
             </ul>
         </nav>
     )
